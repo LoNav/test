8fb2b0f53601bde55edbaecbf8137e73a0da7334
LoNav <62744141+LoNav@users.noreply.github.com>
2021-01-27 15:02:50 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+function toggleEsat(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active')
+
+    var popup = document.getElementById('popupesat');
+    popup.classList.toggle('active')
+}
+function toggleOrmeactivitee(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active')
+    
+    var popup = document.getElementById('popupormeactivitee');
+    popup.classList.toggle('active')
+}
+function toggleSmictom(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active')
+    
+    var popup = document.getElementById('popupsmictom');
+    popup.classList.toggle('active')
+}
